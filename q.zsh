5e67b584aa8b875b5acb33d629ed17e8321d2479
quentinchen727 <quentin.chen727@gmail.com>
2016-11-17 18:50:12 -0800

    Add customized q.zsh file and modified .zshrc file

--- q.zsh
+++ q.zsh
@@ -1,21 +1,8 @@
-# Exports {{{
-export GITHUB_USER="your-username"
+## Exports {{{
+export GITHUB_USER="quentinchen727"
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin # Reorder PATH so local bin is first
-#export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='1;32'
-export MANPAGER="less -X" # Don’t clear the screen after quitting a manual page
 export EDITOR="vim"
-export TERM="screen-256color"
-export CLICOLOR=1
-export LSCOLORS=Gxfxcxdxbxegedabagacad
-export LS_COLORS='di=1;32:fi=0:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=1;36:*.rpm=90:*tar=1;21'
-# }}}
-
-# Ruby {{{
-function get_ruby_version() {
-  ruby -v | awk '{print $1 " " $2}'
-}
-# }}}
+## }}}
 
 # Tmux {{{
 # Makes creating a new tmux session (with a specific name) easier
@@ -35,8 +22,6 @@ function tmuxkill() {
 # }}}
 
 # Alias' {{{
-alias grep='grep --color'
-alias ls='ls -hF --color=tty'
 alias vi="vim"
 alias r="source ~/.zshrc"
 alias tat='tmux new-session -As $(basename "$PWD" | tr . -)' # will attach if session exists, or create a new session
@@ -111,24 +96,24 @@ bindkey '^A' beginning-of-line
 bindkey '^E' end-of-line
 # }}}
 
-# Colours {{{
-autoload colors; colors
-
-# The variables are wrapped in \%\{\%\}. This should be the case for every
-# variable that does not contain space.
-for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
-  eval PR_$COLOR='%{$fg_no_bold[${(L)COLOR}]%}'
-  eval PR_BOLD_$COLOR='%{$fg_bold[${(L)COLOR}]%}'
-done
-
-eval RESET='$reset_color'
-export PR_RED PR_GREEN PR_YELLOW PR_BLUE PR_WHITE PR_BLACK
-export PR_BOLD_RED PR_BOLD_GREEN PR_BOLD_YELLOW PR_BOLD_BLUE
-export PR_BOLD_WHITE PR_BOLD_BLACK
-
-# Clear LSCOLORS
-unset LSCOLORS
-# }}}
+## Colours {{{
+#autoload colors; colors
+#
+## The variables are wrapped in \%\{\%\}. This should be the case for every
+## variable that does not contain space.
+#for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
+#  eval PR_$COLOR='%{$fg_no_bold[${(L)COLOR}]%}'
+#  eval PR_BOLD_$COLOR='%{$fg_bold[${(L)COLOR}]%}'
+#done
+#
+#eval RESET='$reset_color'
+#export PR_RED PR_GREEN PR_YELLOW PR_BLUE PR_WHITE PR_BLACK
+#export PR_BOLD_RED PR_BOLD_GREEN PR_BOLD_YELLOW PR_BOLD_BLUE
+#export PR_BOLD_WHITE PR_BOLD_BLACK
+#
+## Clear LSCOLORS
+#unset LSCOLORS
+## }}}
 
 # Set Options {{{
 # ===== Basics
@@ -175,133 +160,133 @@ setopt transient_rprompt # only show the rprompt on the current prompt
 setopt multios # perform implicit tees or cats when multiple redirections are attempted
 # }}}
 
-# Prompt {{{
-function virtualenv_info {
-  [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
-}
-
-function prompt_char {
-  git branch >/dev/null 2>/dev/null && echo '±' && return
-  hg root >/dev/null 2>/dev/null && echo '☿' && return
-  echo '○'
-}
-
-function box_name {
-  [ -f ~/.box-name ] && cat ~/.box-name || hostname -s
-}
-
-# http://blog.joshdick.net/2012/12/30/my_git_prompt_for_zsh.html
-# copied from https://gist.github.com/4415470
-# Adapted from code found at <https://gist.github.com/1712320>.
-
-#setopt promptsubst
-autoload -U colors && colors # Enable colors in prompt
-
-# Modify the colors and symbols in these variables as desired.
-GIT_PROMPT_SYMBOL="%{$fg[blue]%}±"
-GIT_PROMPT_PREFIX="%{$fg[green]%} [%{$reset_color%}"
-GIT_PROMPT_SUFFIX="%{$fg[green]%}]%{$reset_color%}"
-GIT_PROMPT_AHEAD="%{$fg[red]%}ANUM%{$reset_color%}"
-GIT_PROMPT_BEHIND="%{$fg[cyan]%}BNUM%{$reset_color%}"
-GIT_PROMPT_MERGING="%{$fg_bold[magenta]%}⚡︎%{$reset_color%}"
-GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}u%{$reset_color%}"
-GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}m%{$reset_color%}"
-GIT_PROMPT_STAGED="%{$fg_bold[green]%}s%{$reset_color%}"
-
-# Show Git branch/tag, or name-rev if on detached head
-function parse_git_branch() {
-  (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
-}
-
-# Show different symbols as appropriate for various Git repository states
-function parse_git_state() {
-  # Compose this value via multiple conditional appends.
-  local GIT_STATE=""
-
-  local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_AHEAD" -gt 0 ]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
-  fi
-
-  local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_BEHIND" -gt 0 ]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
-  fi
-
-  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
-  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
-  fi
-
-  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
-  fi
-
-  if ! git diff --quiet 2> /dev/null; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
-  fi
-
-  if ! git diff --cached --quiet 2> /dev/null; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
-  fi
-
-  if [[ -n $GIT_STATE ]]; then
-    echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
-  fi
-}
-
-# If inside a Git repository, print its branch and state
-function git_prompt_string() {
-  local git_where="$(parse_git_branch)"
-  [ -n "$git_where" ] && echo "on %{$fg[blue]%}${git_where#(refs/heads/|tags/)}$(parse_git_state)"
-}
-
-function current_pwd {
-  echo $(pwd | sed -e "s,^$HOME,~,")
-}
-
-# Original prompt with User name and Computer name included...
-# PROMPT='
-# ${PR_GREEN}%n%{$reset_color%} %{$FG[239]%}at%{$reset_color%} ${PR_BOLD_BLUE}$(box_name)%{$reset_color%} %{$FG[239]%}in%{$reset_color%} ${PR_BOLD_YELLOW}$(current_pwd)%{$reset_color%} $(git_prompt_string)
-# $(prompt_char) '
-
-PROMPT='
-${PR_GREEN}M.%{$reset_color%} ${PR_BOLD_YELLOW}$(current_pwd)%{$reset_color%} $(git_prompt_string)
-$(prompt_char) '
-
-export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [(y)es (n)o (a)bort (e)dit]? "
-
-#RPROMPT='${PR_GREEN}$(virtualenv_info)%{$reset_color%} ${PR_RED}$(get_ruby_version)%{$reset_color%}'
-# }}}
-
+## Prompt {{{
+#function virtualenv_info {
+#  [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
+#}
+#
+#function prompt_char {
+#  git branch >/dev/null 2>/dev/null && echo '±' && return
+#  hg root >/dev/null 2>/dev/null && echo '☿' && return
+#  echo '○'
+#}
+#
+#function box_name {
+#  [ -f ~/.box-name ] && cat ~/.box-name || hostname -s
+#}
+#
+## http://blog.joshdick.net/2012/12/30/my_git_prompt_for_zsh.html
+## copied from https://gist.github.com/4415470
+## Adapted from code found at <https://gist.github.com/1712320>.
+#
+##setopt promptsubst
+#autoload -U colors && colors # Enable colors in prompt
+#
+## Modify the colors and symbols in these variables as desired.
+#GIT_PROMPT_SYMBOL="%{$fg[blue]%}±"
+#GIT_PROMPT_PREFIX="%{$fg[green]%} [%{$reset_color%}"
+#GIT_PROMPT_SUFFIX="%{$fg[green]%}]%{$reset_color%}"
+#GIT_PROMPT_AHEAD="%{$fg[red]%}ANUM%{$reset_color%}"
+#GIT_PROMPT_BEHIND="%{$fg[cyan]%}BNUM%{$reset_color%}"
+#GIT_PROMPT_MERGING="%{$fg_bold[magenta]%}⚡︎%{$reset_color%}"
+#GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}u%{$reset_color%}"
+#GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}m%{$reset_color%}"
+#GIT_PROMPT_STAGED="%{$fg_bold[green]%}s%{$reset_color%}"
+#
+## Show Git branch/tag, or name-rev if on detached head
+#function parse_git_branch() {
+#  (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
+#}
+#
+## Show different symbols as appropriate for various Git repository states
+#function parse_git_state() {
+#  # Compose this value via multiple conditional appends.
+#  local GIT_STATE=""
+#
+#  local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+#  if [ "$NUM_AHEAD" -gt 0 ]; then
+#    GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
+#  fi
+#
+#  local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
+#  if [ "$NUM_BEHIND" -gt 0 ]; then
+#    GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
+#  fi
+#
+#  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+#  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+#    GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
+#  fi
+#
+#  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+#    GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
+#  fi
+#
+#  if ! git diff --quiet 2> /dev/null; then
+#    GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
+#  fi
+#
+#  if ! git diff --cached --quiet 2> /dev/null; then
+#    GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
+#  fi
+#
+#  if [[ -n $GIT_STATE ]]; then
+#    echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
+#  fi
+#}
+#
+## If inside a Git repository, print its branch and state
+#function git_prompt_string() {
+#  local git_where="$(parse_git_branch)"
+#  [ -n "$git_where" ] && echo "on %{$fg[blue]%}${git_where#(refs/heads/|tags/)}$(parse_git_state)"
+#}
+#
+#function current_pwd {
+#  echo $(pwd | sed -e "s,^$HOME,~,")
+#}
+#
+## Original prompt with User name and Computer name included...
+## PROMPT='
+## ${PR_GREEN}%n%{$reset_color%} %{$FG[239]%}at%{$reset_color%} ${PR_BOLD_BLUE}$(box_name)%{$reset_color%} %{$FG[239]%}in%{$reset_color%} ${PR_BOLD_YELLOW}$(current_pwd)%{$reset_color%} $(git_prompt_string)
+## $(prompt_char) '
+#
+#PROMPT='
+#${PR_GREEN}M.%{$reset_color%} ${PR_BOLD_YELLOW}$(current_pwd)%{$reset_color%} $(git_prompt_string)
+#$(prompt_char) '
+#
+#export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [(y)es (n)o (a)bort (e)dit]? "
+#
+##RPROMPT='${PR_GREEN}$(virtualenv_info)%{$reset_color%} ${PR_RED}$(get_ruby_version)%{$reset_color%}'
+## }}}
+#
 # History {{{
 HISTSIZE=10000
 SAVEHIST=9000
 HISTFILE=~/.zsh_history
 # }}}
 
-# Zsh Hooks {{{
-function precmd {
-  # vcs_info
-  # Put the string "hostname::/full/directory/path" in the title bar:
-  echo -ne "\e]2;$PWD\a"
-
-  # Put the parentdir/currentdir in the tab
-  echo -ne "\e]1;$PWD:h:t/$PWD:t\a"
-}
-
-function set_running_app {
-  printf "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a"
-}
-
-function preexec {
-  set_running_app
-}
-
-function postexec {
-  set_running_app
-}
-# }}}
+## Zsh Hooks {{{
+#function precmd {
+#  # vcs_info
+#  # Put the string "hostname::/full/directory/path" in the title bar:
+#  echo -ne "\e]2;$PWD\a"
+#
+#  # Put the parentdir/currentdir in the tab
+#  echo -ne "\e]1;$PWD:h:t/$PWD:t\a"
+#}
+#
+#function set_running_app {
+#  printf "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a"
+#}
+#
+#function preexec {
+#  set_running_app
+#}
+#
+#function postexec {
+#  set_running_app
+#}
+## }}}
 # ssh-agent {{{
 SSH_ENV="$HOME/.ssh/environment"
 function start_agent {
