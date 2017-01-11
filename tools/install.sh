d34a3cb7350576f6c6633b69cad41f0506d10c33
quentinchen727 <quentin.chen727@gmail.com>
2017-01-10 19:10:13 -0800

    change from sh to zsh

--- tools/install.sh
+++ tools/install.sh
@@ -7,9 +7,9 @@ cd
 git clone $CONFIG_REPO $CONFIG_DIR
 ln -sf ~/$CONFIG_DIR/vimrc ~/.vimrc
 ln -sf ~/$CONFIG_DIR/tmux.conf ~/.tmux.conf
-ln -sf ~/$CONFIG_DIR/Zeus.sh ~/.oh-my-zsh/custom/Zeus.sh
+ln -sf ~/$CONFIG_DIR/Zeus.zsh ~/.oh-my-zsh/custom/Zeus.zsh
 touch sisyphus.zsh
-ln -sf sisyphus.zsh ~/.oh-my-zsh/custom/sisyphus.zsh
+ln -sf ~/sisyphus.zsh ~/.oh-my-zsh/custom/sisyphus.zsh
 
 #clone vim plugins and bundles
 cd ~
