8de16dd51dcee240e1ffc31dd1c112ca8a57db32
quentinchen727 <quentin.chen727@gmail.com>
2017-01-10 18:29:54 -0800

    typo

--- tools/install.sh
+++ tools/install.sh
@@ -1,13 +1,13 @@
 #! env zsh
 CONFIG_DIR=".zsh_tmux_vim_repo"
-CONFIG_REPO="https://github.com/quentin.chen727/zsh_tmux_vim_repo.git"
+CONFIG_REPO="https://github.com/quentinchen727/zsh_tmux_vim_repo.git"
 VIM_DIR=".vim"
 VIM_REPO="https://github.com/quentinchen727/Vim_plugins_repo.git"
 cd
 git clone $CONFIG_REPO $CONFIG_DIR
 ln -s $CONFIG_DIR/vimrc ~/.vimrc
-ln -s $CONFIG_DIR/vtmux.conf ~/.tmux.conf
-ln -s $CONFIG_DIR/vZeus.sh .oh-my-zsh/custom/Zeus.sh
+ln -s $CONFIG_DIR/tmux.conf ~/.tmux.conf
+ln -s $CONFIG_DIR/Zeus.sh .oh-my-zsh/custom/Zeus.sh
 touch sisyphus.zsh
 ln -s sisyphus.zsh .oh-my-zsh/custom/sisyphus.zsh
 
