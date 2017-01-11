46dfd7302c666895e987ca1ccb6b48e2b6f43d1a
quentinchen727 <quentin.chen727@gmail.com>
2017-01-10 18:42:14 -0800

    change F to f

--- tools/install.sh
+++ tools/install.sh
@@ -5,11 +5,11 @@ VIM_DIR=".vim"
 VIM_REPO="https://github.com/quentinchen727/Vim_plugins_repo.git"
 cd
 git clone $CONFIG_REPO $CONFIG_DIR
-ln -sF $CONFIG_DIR/vimrc ~/.vimrc
-ln -sF $CONFIG_DIR/tmux.conf ~/.tmux.conf
-ln -sF $CONFIG_DIR/Zeus.sh .oh-my-zsh/custom/Zeus.sh
+ln -sf $CONFIG_DIR/vimrc ~/.vimrc
+ln -sf $CONFIG_DIR/tmux.conf ~/.tmux.conf
+ln -sf $CONFIG_DIR/Zeus.sh .oh-my-zsh/custom/Zeus.sh
 touch sisyphus.zsh
-ln -sF sisyphus.zsh .oh-my-zsh/custom/sisyphus.zsh
+ln -sf sisyphus.zsh .oh-my-zsh/custom/sisyphus.zsh
 
 #clone vim plugins and bundles
 cd ~
